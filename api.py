2f61c4ae703ec45f81250cdab397b527b3b057b2
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-02-15 13:49:32 -0300

    move main logic to api.py

---
+++ api.py
@@ -0,0 +1,9 @@
+
+
+class API(object):
+
+    def __call__(self, environ, start_response):
+        response_body = b'Hello World'
+        status = '200 OK'
+        start_response(status, headers=[])
+        return iter([response_body])
--- app.py
+++ app.py
@@ -1,24 +1,4 @@
-from wsgiref import simple_server
+from api import API
 
 
-class Reverseware(object):
-
-    def __init__(self, app):
-        self.wrapped_app = app
-
-    def __call__(self, environ, start_response, *args, **kwargs):
-        wrapped_app_response = self.wrapped_app(environ, start_response)
-        return [data[::-1] for data in wrapped_app_response]
-
-
-def app(environ, start_response):
-    response_body = [f'{k}: {v}' for k, v in sorted(environ.items())]
-    response_body = '\n'.join(response_body)
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain'), ]
-    start_response(status, response_headers)
-    return [response_body.encode('utf8')]
-
-
-with simple_server.make_server('localhost', 8000, Reverseware(app)) as server:
-    server.serve_forever()
+app = API()
