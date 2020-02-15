0b7c226ac46d87d3bf808d69b9a0bf84e9c11700
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-02-15 14:25:52 -0300

    implement routes

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ import webob
 
 class API(object):
 
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, environ, start_response):
         request = webob.Request(environ)
 
@@ -10,13 +13,31 @@ class API(object):
 
         return response(environ, start_response)
 
-    def handle_request(self, request):
-        user_agent = request.environ.get(
-            'HTTP_USER_AGENT',
-            'No User Agent Found'
-        )
+    def route(self, path):
 
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
+
+    def handle_request(self, request):
         response = webob.Response()
-        response.text = f'Hello, my friend with this user agent: {user_agent}'
+
+        handler = self.find_handler(request.path)
+
+        if handler is not None:
+            handler(request, response)
+        else:
+            self.default_response(response)
 
         return response
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            if path == request_path:
+                return handler
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = 'Not found.'
--- app.py
+++ app.py
@@ -2,3 +2,13 @@ from api import API
 
 
 app = API()
+
+
+@app.route('/')
+def index(request, response):
+    response.text = 'Hello from index page'
+
+
+@app.route('/about')
+def about(request, response):
+    response.text = 'Hello from about page'
