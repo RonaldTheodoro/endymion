df871b69078d14fbc5c8f7cf595bafa917b4e96b
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-02-15 13:59:06 -0300

    implement the handle_request method

--- api.py
+++ api.py
@@ -6,7 +6,17 @@ class API(object):
     def __call__(self, environ, start_response):
         request = webob.Request(environ)
 
-        response = webob.Response()
-        response.text = 'Hello World'
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get(
+            'HTTP_USER_AGENT',
+            'No User Agent Found'
+        )
+
+        response = webob.Response()
+        response.text = f'Hello, my friend with this user agent: {user_agent}'
+
+        return response
