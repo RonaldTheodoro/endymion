9b1b76745634c725cec504ac9dbf7d0d44b3d4e1
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-02-16 15:24:04 -0300

    Add test_route_overlap_throws_exception

--- tests/test_endymion.py
+++ tests/test_endymion.py
@@ -1,7 +1,17 @@
+import pytest
+
 
 def test_basic_route_adding(app):
-    @app.route('/')
+
+
+def test_route_overlap_throws_exception(app):
+    @app.route('/home')
     def index(request):
         return 'YOLO'
 
-    assert '/' in app.routes
+    assert '/home' in app.routes
+
+    with pytest.raises(AssertionError):
+        @app.route('/home')
+        def index02(request):
+            return 'YOLO'
