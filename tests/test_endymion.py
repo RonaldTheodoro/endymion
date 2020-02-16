5be1eab57e2e6f2812c017dc3b64a5dc795e389b
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-02-16 15:24:15 -0300

    Change path

--- tests/test_endymion.py
+++ tests/test_endymion.py
@@ -2,6 +2,11 @@ import pytest
 
 
 def test_basic_route_adding(app):
+    @app.route('/home')
+    def index(request):
+        return 'YOLO'
+
+    assert '/home' in app.routes
 
 
 def test_route_overlap_throws_exception(app):
